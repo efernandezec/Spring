c0ef33e3ad2a2583a7e97643f9cefc375e1a524f
Estiben Fernández Echeverri <efernandeze@unal.edu.co>
2024-12-19 21:05:55 -0500

    Pasar información  a la vista de forma dinamica

--- springboot-web/src/main/java/com/estiben/curso/springboot/webapp/springboot_web/UserController.java
+++ springboot-web/src/main/java/com/estiben/curso/springboot/webapp/springboot_web/UserController.java
@@ -1,6 +1,7 @@
 package com.estiben.curso.springboot.webapp.springboot_web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
-        
+    public String details(Model model){
+        model.addAttribute("title", "Hola mundo from Spring!!");
+        model.addAttribute("name", "Estiben");
+        model.addAttribute("lastName", "Fernández");
         return "details";
     }
 
