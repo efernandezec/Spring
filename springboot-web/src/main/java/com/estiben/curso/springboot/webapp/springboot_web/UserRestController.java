89bb40516a01ba86070f40f3743fd8877579777c
Estiben Fernández Echeverri <efernandeze@unal.edu.co>
2024-12-19 21:40:21 -0500

    RequestMapping en RestController

--- springboot-web/src/main/java/com/estiben/curso/springboot/webapp/springboot_web/UserRestController.java
+++ springboot-web/src/main/java/com/estiben/curso/springboot/webapp/springboot_web/UserRestController.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 public class UserRestController {
 
     @GetMapping("details2")
